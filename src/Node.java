ba247b6907dc99089a5779d51eef6f391f1e4f5a
jakew <jakew@SUCCMaster-9000>
2020-04-20 13:36:17 -0700

    First upload

--- src/Node.java
+++ src/Node.java
@@ -31,11 +31,19 @@ public class Node {
     public int getValue(){
         return val;
     }
-            
+    
+    
+    public void setLeft(Node l){
+        lChild = l;
+    }
+    public void setRight(Node r){
+        rChild = r;
+    }
+    
     public static boolean compareTo(Node n1, Node n2){
         return (n1.toString() == n2.toString() && n1.getValue() == n2.getValue());
     }
-    public void add(){
+    protected void add(){
         val++;
     }
     public static boolean compareChildren(Node n1, Node n2){ //node n1 is to the left in the tree, node n2 is to the right in the tree, so the left child's right node and the right child's left node are being compared
--- src/TreeBall.java
+++ src/TreeBall.java
@@ -12,18 +12,35 @@ public class TreeBall {
     private Node head;
     private final int levels = 10;
     public TreeBall(){
-        
+        createTree();
     }
     private void createTree(){
         //this will have a default of 10 levels, and the trees will dual reference the children they share
         head = new Node();
-        for(int i = 1; i <= levels; i++){
+        Node[] previouslevel = new Node[1];
+        previouslevel[0] = head;
+        Node[] setup = null;
+        for(int i = 2; i <= levels + 1; i++){
+            setup = new Node[i];
+            for(int j = 0; j < i; j++){
+                setup[j] = new Node();
+            }
+            previouslevel[0].setLeft(setup[0]);
+            previouslevel[0].setRight(setup[1]);
+            for(int c = 1; c < previouslevel.length; c++){
+                previouslevel[c].setLeft(setup[c]);
+                previouslevel[c].setRight(setup[c + 1]);
+            }
+            previouslevel[previouslevel.length - 1].setLeft(setup[setup.length - 2]);
+            previouslevel[previouslevel.length - 1].setRight(setup[setup.length - 1]);
             
+            previouslevel = setup;
         }
         //use a bitstring that resets at the next level to update the bitstring
     }
     
-    private String createBitString(int level){
+    
+    private static String createBitString(int level){
         String r = Integer.toBinaryString(level);
         while(r.length() < level){
             r = '0' + r;
@@ -31,8 +48,23 @@ public class TreeBall {
         return r;
     }
     
+    private Node locateFromTree(int num, int level){
+        if(levels <= level){ return null; }
+        //level--;
+        Node r = head;
+        while(level > -1){
+            if(level > num){
+                r = r.getLeftChild();
+            }else{
+                r = r.getRightChild();
+            }
+            level--;
+        }
+        return r;
+    }
+    
     public void ProcessData(String Bitstring){
-        head.add();
+        //head.add();
         Node t = head;
         while(Bitstring.length() > 0){
             if(Bitstring.charAt(0) == 'L'){
@@ -48,5 +80,26 @@ public class TreeBall {
             }
             Bitstring = Bitstring.substring(1);
         }
+        head.add();
     }
+    
+    @Override
+    public String toString(){
+        String r = "";
+        for(int i = 0; i < levels; i++){
+            
+            for(int l = 0; l < levels - i - 1; l++){
+                    r += '\t';
+                }
+            for(int c = 0; c <= i; c++){
+                if(locateFromTree(c, i) == null){ r += "null \t\t"; }else{
+                r += locateFromTree(c, i).getValue() + "\t\t";
+                }
+            }
+            r += '\n';
+            //System.out.println(i + ", \n" + r);
+        }
+        return r;
+    }
+    
 }
